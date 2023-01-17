5617a8777ad73b9dc96dcd255051ee263074c489
naeemahmed1994 <ahmednaeemnafi@gmail.com>
2023-01-17 13:37:14 +0600

    add spinning/input key handler

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
-const loadPhones = async (searchText) =>{
+const loadPhones = async (searchText, dataLimit) =>{
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     const res = await fetch(url)
     const data = await res.json()
-    displayPhones(data.data);
+    displayPhones(data.data, dataLimit);
 }
 
 
-const displayPhones = phones =>{
+const displayPhones = (phones, dataLimit) =>{
     // console.log(phones);
     const phonesContainer = document.getElementById('phone-container');
     phonesContainer.textContent = '';
     // display 10 phones only
-    phones = phones.slice(0,10);
+    const showAll = document.getElementById('show-all');
+    if(dataLimit && phones.length > 10){
+        phones = phones.slice(0,10);
+        showAll.classList.remove('d-none');
+    }else{
+        showAll.classList.add('d-none');
+    }
     // display no phone found
     const noPhone = document.getElementById('no-found-message');
     if(phones.length === 0){
@@ -29,17 +35,53 @@ const displayPhones = phones =>{
             <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
-                <p class="card-text">${phone.slug}</p>
+                <p class="card-text">lorem ipsum lorem lorem ipsum super ipsum</p>
+                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details</button>
             </div>
         </div>
         `;
         phonesContainer.appendChild(div);
-    })
+    });
+    //stop spinner/loader
+    toggleSpinner(false);
 }
-document.getElementById('btn-search').addEventListener('click', function(){
+const processSearch = (dataLimit) =>{
+    toggleSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    loadPhones(searchText);
-    searchField.value = '';
+    loadPhones(searchText, dataLimit);
+}
+//handle search button click
+document.getElementById('btn-search').addEventListener('click', function(){
+    // start spinnig/loader
+    processSearch(10);
+});
+
+//search input field enter key handler
+document.getElementById('search-field').addEventListener('keypress', function (e) {
+    // console.log(e.key);
+    if (e.key === 'Enter') {
+        processSearch(10);
+    }
+});
+
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none');
+    }else{
+        loaderSection.classList.add('d-none');
+    }
+}
+//It is not good way to show all
+document.getElementById('btn-show-all').addEventListener('click',function(){
+    processSearch();
 })
+const loadPhoneDetails = async id =>{
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data.data);
+}
+
 // loadPhones();
